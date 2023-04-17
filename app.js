78e7f4c9ef69ca0eecf94bf857d623094ae54a67
Shreyash Santosh Shahu <sahushreyash48@gmail.com>
2023-04-17 23:05:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+function update(){
+    let str="";
+    tableBody = document.getElementById('tbody')
+    ItemJsonArrayStr = localStorage.getItem('ItemJson');
+    ItemJsonArray = JSON.parse(ItemJsonArrayStr); 
+    if(ItemJsonArray != null){
+        ItemJsonArray.forEach((element,index)=>{
+            str += `
+            <div class="row">
+              <div class="c1">${index+1}</div>
+              <div class="c2">${element[0]}</div>
+              <div class="c2">${element[1]}</div>
+              <div class="c1"><button onclick="Delete(${index})" class="delbtn">Delete</button></div>
+            </div>
+            `
+        })
+        tableBody.innerHTML = str;
+    }
+}
+function Adding(){
+    console.log("Appending list...") 
+    tit= document.getElementById("title").value;
+    desc = document.getElementById("description").value;
+    if(localStorage.getItem('ItemJson')==null){
+        ItemJsonArray = [];
+        ItemJsonArray.push([tit, desc])
+        localStorage.setItem('ItemJson', JSON.stringify(ItemJsonArray))
+    }
+    else{
+        ItemJsonArrayStr = localStorage.getItem('ItemJson');
+        ItemJsonArray = JSON.parse(ItemJsonArrayStr);
+        ItemJsonArray.push([tit, desc]);
+        localStorage.setItem('ItemJson',JSON.stringify(ItemJsonArray));
+    }
+
+    //add elements to the table
+    update()
+}
+add = document.getElementById("add");
+add.addEventListener("click",Adding)
+update()//initially updating the data
+
+function Delete(itemIndex){
+    //deleting the element from the local storage of the pc
+    console.log("Deleting...",itemIndex)
+    ItemJsonArrayStr = localStorage.getItem('ItemJson');
+    ItemJsonArray = JSON.parse(ItemJsonArrayStr);
+    ItemJsonArray.forEach((element,index)=>{
+        if(index==itemIndex){
+            ItemJsonArray.splice(itemIndex,1);
+        }
+    })
+    localStorage.setItem('ItemJson',JSON.stringify(ItemJsonArray));
+
+    //To remove the element from table
+    update()
+}
